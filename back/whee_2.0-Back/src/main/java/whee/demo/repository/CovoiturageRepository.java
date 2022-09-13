941e8992a572c030930542ef101f47020c2bb42b
Miora <m.andriamanjay@yahoo.com>
2022-09-13 17:24:52 +0300

    tambatra

---
+++ back/whee_2.0-Back/src/main/java/whee/demo/repository/CovoiturageRepository.java
@@ -0,0 +1,9 @@
+package whee.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import whee.demo.entity.Covoiturage;
+
+@Repository
+public interface CovoiturageRepository extends JpaRepository<Covoiturage,Long> {
+}
